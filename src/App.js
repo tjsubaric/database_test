759c304c7beff88405a94b6b28dc084a4214dc33
tjsubaric <91640793+tsubaric@users.noreply.github.com>
2023-02-12 14:35:37 -0600

    token api

--- src/App.js
+++ src/App.js
@@ -1,21 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Dashboard from './components/Dashboard';
 import Preferences from './components/Preferences';
+import Login from './components/Login'
 
 function App() {
+  const [token, setToken] = useState();
+
+  if(!token) {
+    return <Login setToken={ setToken } />
+  }
+  
   return (
     <div className="wrapper">
       <h1>Application</h1>
       <BrowserRouter>
         <Routes>
-          <Route path="/dashboard">
-            <Dashboard />
-          </Route>
-          <Route path="/preferences">
-            <Preferences />
-          </Route>
+        <Route path='/dashboard' exact component={Dashboard} />
+        <Route path='/preferences' exact component={Preferences} />
         </Routes>
       </BrowserRouter>
     </div>
