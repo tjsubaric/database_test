6e4059dd11eada7e19d91a061b23ead26c405e09
tjsubaric <91640793+tsubaric@users.noreply.github.com>
2023-02-12 15:12:41 -0600

    finished tutorial

--- src/App.js
+++ src/App.js
@@ -1,17 +1,28 @@
-import React, { useState } from 'react';
+import React from 'react';
 import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Dashboard from './components/Dashboard';
 import Preferences from './components/Preferences';
 import Login from './components/Login'
+import useToken from './components/useToken';
+
+function setToken(userToken){
+  sessionStorage.setItem('token', JSON.stringify(userToken));
+}
+
+function getToken(){
+  const tokenString = sessionStorage.getItem('token')
+  const userToken = JSON.parse(tokenString);
+  return userToken?.token
+}
 
 function App() {
-  const [token, setToken] = useState();
+  const {token, setToken } = useToken();
 
   if(!token) {
     return <Login setToken={ setToken } />
   }
-  
+
   return (
     <div className="wrapper">
       <h1>Application</h1>
